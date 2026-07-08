e05a09b0df26173de9b753e4986fddd338ec7a95
chivan-kats41 <chivankats@gmail.com>
2026-07-08 17:16:38 +0700

    settings updated once again

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -6,6 +6,12 @@ DEBUG = False
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'cybertechacademy.pythonanywhere.com').split(',')
 
 # PostgreSQL for production
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
 
 
 # Security settings
